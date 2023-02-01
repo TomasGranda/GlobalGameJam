6c1d09d9bcee6329714555ce632931fecc0428f0
alejandro-Gonza <64438096+alejandro-Gonza@users.noreply.github.com>
2023-02-01 20:27:05 -0300

    :heavy_check_mark: View Path System :laughing:

--- Assets/Scripts/Ai/Base Enemy/Entity.cs
+++ Assets/Scripts/Ai/Base Enemy/Entity.cs
@@ -6,16 +6,35 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Entity : MonoBehaviour
 {
-    public StateMachine stateMachine = new StateMachine();
+    [Header("Parameters")]
+    public EntityStats stats;
 
+    [Header("Ruta")]
+    public List<Transform> wayPoints = new List<Transform>();
+
+
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    public StateMachine stateMachine = new StateMachine();
     public NavMeshAgent agent { get; private set; }
 
+    #region States
     private IdleState idle;
     private MoveState move;
     private AttackState attack;
     private PatrolState patrol;
     private DetectionState detection;
     private DeathState death;
+    #endregion
+
+    #region Custom Gizmo
+
+    [Header("Settings Gizmo")]
+    [Tooltip("Cambia el color de la linea de patrulla")]
+    public Color colorLine = new Color(0, 0, 0, 1);
+
+    #endregion
 
     public virtual void Start()
     {
@@ -43,5 +62,48 @@ public class Entity : MonoBehaviour
     public virtual void SetChangeState()
     {
         stateMachine.Init(patrol);
+    }
+
+    public int counterIndex { get; private set; }
+
+    public void FollowPath()
+    {
+        if (counterIndex <= wayPoints.Count - 1)
+        {
+            transform.position += transform.forward * stats.moveSpeed * Time.deltaTime;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(wayPoints[counterIndex].transform.position - transform.position), 1 * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, wayPoints[counterIndex].transform.position) < 1f)
+            {
+                counterIndex++;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (wayPoints.Count > 0)
+        {
+            Gizmos.color = colorLine;
+
+            for (int i = 0; i < wayPoints.Count - 1; i++)
+            {
+                Gizmos.DrawLine(wayPoints[i].position, wayPoints[i + 1].position);
+
+                Vector3 start = wayPoints[i].position;
+
+                Vector3 end = wayPoints[i + 1].position;
+
+                Vector3 arrowTip = end;
+
+                Vector3 left = end - Quaternion.LookRotation(Vector3.forward) * (Quaternion.Euler(0, -50, 0) * (end - start).normalized);
+                Vector3 right = end - Quaternion.LookRotation(Vector3.forward) * (Quaternion.Euler(0, 50, 0) * (end - start).normalized);
+
+                Gizmos.DrawLine(arrowTip, left);
+                Gizmos.DrawLine(arrowTip, right);
+            }
+        }
+
     }
 }
--- Assets/Scripts/Ai/Base Enemy/States/PatrolState.cs
+++ Assets/Scripts/Ai/Base Enemy/States/PatrolState.cs
@@ -6,25 +6,23 @@ public class PatrolState : BaseStateMachineState
 {
     private readonly Entity controller;
 
-    private Vector3 nextPosition;
-
     public PatrolState(Entity controller)
     {
         this.controller = controller;
     }
 
-    public override void ExecuteState()
-    {
-        throw new System.NotImplementedException();
-    }
-
     public override void OnEnterState(params object[] objects)
     {
-        Debug.Log("asd");
+
+    }
+
+    public override void ExecuteState()
+    {
+        controller.FollowPath();
     }
 
     public override void OnExitState()
     {
-        throw new System.NotImplementedException();
+
     }
 }
